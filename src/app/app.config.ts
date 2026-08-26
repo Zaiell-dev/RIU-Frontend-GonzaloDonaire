6276d19b99615bd3a43d824c605c536ae7c7af8a
Gonzalo Donaire <gonzaloarield5@gmail.com>
2026-08-26 11:25:28 -0300

    refactor: move hero initialization to app configuration and remove the redundant FAB button

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HeroesService } from './heroes/services/heroes-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideAppInitializer(() => {
+      inject(HeroesService);
+    }),
+    provideRouter(routes),
+  ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,33 +1,16 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  signal,
-} from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { HeroesService } from './heroes/services/heroes-service';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { LoadingService } from './heroes/services/loading-service';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatButtonModule,
-    MatIconModule,
-    RouterLink,
-    Header,
-    MatProgressSpinner,
-  ],
+  imports: [RouterOutlet, Header, MatProgressSpinner],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  protected readonly title = signal('RIU-Frontend-GonzaloDonaire');
-  loadingService = inject(LoadingService);
-  heroesService = inject(HeroesService);
+  protected readonly loadingService = inject(LoadingService);
 }
