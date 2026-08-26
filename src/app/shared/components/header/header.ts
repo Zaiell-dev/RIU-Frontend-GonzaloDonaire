5f0940fe090048679324b979711249b5c31067fd
Gonzalo Donaire <gonzaloarield5@gmail.com>
2026-08-26 11:03:03 -0300

    perf: remove unused Header dependencies

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,18 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [
-    RouterLink,
-    RouterLinkActive,
-    MatButtonModule,
-    MatListModule,
-    MatIconModule,
-  ],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
